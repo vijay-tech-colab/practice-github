9efe8bc5c7c90356d56e668b1ca632c48ac023a9
vijay kumar <vijaykumar06232@gmail.com>
2024-09-16 11:28:42 +0530

    index.js file added

---
+++ students contribution/index.js
@@ -0,0 +1,9 @@
+function Student(name,age,email){
+    this.name = name;
+    this.age = age;
+    this.email = email;
+}
+
+
+const newObj = new Student('vijay kumar',20,'vijaykumar7868@gmail.com');
+console.log(newObj.name);
